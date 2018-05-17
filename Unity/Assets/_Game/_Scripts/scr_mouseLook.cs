26f67ee7a4dc3d9b30af2a6deb4f0c2c34bcf960
Tobias <hedgefoggames@outlook.com>
2018-05-17 14:46:34 +0200

    Update 0.6

---
+++ Unity/Assets/_Game/_Scripts/scr_mouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_mouseLook : MonoBehaviour {
+
+    // Initialize the public variables
+    public float mouseSensitivityX;
+    public float mouseSensitivityY;
+
+    // Initialize the private variables
+    private float rotationX;
+    private float rotationY;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        rotationX -= Input.GetAxis("Mouse Y") * mouseSensitivityY;
+        rotationY += Input.GetAxis("Mouse X") * mouseSensitivityX;
+
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, transform.eulerAngles.z);
+	}
+}
