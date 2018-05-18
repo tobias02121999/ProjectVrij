9c072ffd66a427adcd81fa44127f6f653a702fe7
tobias02121999 <hedgefoggames@outlook.com>
2018-05-18 18:32:52 +0200

    Update 0.9

--- Unity/Assets/scr_blinkLight.cs
+++ Unity/Assets/scr_blinkLight.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class scr_blinkLight : MonoBehaviour {
 
+    // Initialize the public variables
+    public float alarmDurationOff;
+    public float alarmDurationOn;
+    public Light _light;
+
+    // Initialize the private variables
+    private float alarm;
+    private bool isOn;
+
 	// Use this for initialization
 	void Start () 
     {
-		
+        alarm = alarmDurationOff;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-		
+        alarm--;
+
+        if (alarm <= 0f)
+        {
+            if (isOn)
+            {
+                isOn = false;
+                alarm = alarmDurationOff;
+            }
+            else
+            {
+                isOn = true;
+                alarm = alarmDurationOn;
+            }
+        }
+
+        _light.enabled = isOn;
 	}
 }
