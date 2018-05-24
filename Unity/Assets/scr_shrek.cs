44c66e29d842b00cf88c4090079ad433e6078ae8
tobias02121999 <hedgefoggames@outlook.com>
2018-05-24 11:23:34 +0200

    Update 1.2

---
+++ Unity/Assets/scr_shrek.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_shrek : MonoBehaviour {
+
+    // Initialize the public variables
+    public float movementSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += transform.forward * movementSpeed;
+	}
+}
