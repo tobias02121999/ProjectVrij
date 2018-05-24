29e391ea15c02562bb59e79ab5f6c2fad0f918a2
tobiasBrugel <hedgefoggames@outlook.com>
2018-05-24 16:35:47 +0200

    Update 1.8

---
+++ Unity/Assets/_Game/_Scripts/scr_button.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_button : MonoBehaviour {
+
+    // Initialize the public variables
+    public GameObject buttonStatePressed;
+    public GameObject buttonStateReleased;
+
+    // Initialize the private variables
+    private bool activated;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (activated)
+        {
+            buttonStatePressed.SetActive(true);
+            buttonStateReleased.SetActive(false);
+        }
+        else
+        {
+            buttonStatePressed.SetActive(false);
+            buttonStateReleased.SetActive(true);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hands")
+        {
+            activated = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Hands")
+        {
+            activated = false;
+        }
+    }
+}
