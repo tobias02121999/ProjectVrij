e80f790940bc2f218438394165b843949e33bdb1
rkrijgsman <robinkrijgsman96@gmail.com>
2018-05-25 17:24:51 +0200

    Update 2.6.5
    hover added

--- Unity/Assets/_Game/_Scripts/scr_hover.cs
+++ Unity/Assets/_Game/_Scripts/scr_hover.cs
@@ -2,20 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scr_hover : MonoBehaviour {
+// Makes objects float up & down while gently spinning.
+public class scr_hover : MonoBehaviour
+{
+    // User Inputs
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
 
-    // Initialize the public variables
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
 
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin object around Y-Axis
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
     }
 }
