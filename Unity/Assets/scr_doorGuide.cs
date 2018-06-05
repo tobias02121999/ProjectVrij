c115828d0081e07633cf3d222de3c6a158ea4199
tobias02121999 <hedgefoggames@outlook.com>
2018-06-05 20:44:42 +0200

    Update 5.0

--- Unity/Assets/scr_doorGuide.cs
+++ Unity/Assets/scr_doorGuide.cs
@@ -7,22 +7,27 @@ public class scr_doorGuide : MonoBehaviour {
     // Initialize the public variables
     public GameObject guide;
     public float doorOpenSpeed;
+    public float doorCloseSpeed;
     public float doorOpenRange;
+    public float doorCloseAlarmDuration;
     public int waypointTriggerId;
+    public Component script;
 
     // Initialize the private variables
     private float currentYRotation = 180;
+    private float doorCloseAlarm;
+    private bool isOpened;
 
 	// Use this for initialization
 	void Start ()
     {
-        
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (guide.GetComponent<scr_pathFinder>().currentWaypoint > waypointTriggerId)
+        if (guide.GetComponent<scr_pathFinder>().currentWaypoint > waypointTriggerId && !isOpened)
         {
             if (doorOpenRange > currentYRotation + doorOpenSpeed)
             {
@@ -33,7 +38,33 @@ public class scr_doorGuide : MonoBehaviour {
             {
                 currentYRotation -= doorOpenSpeed;
             }
+
+            if (!(doorOpenRange > currentYRotation + doorOpenSpeed) && !(doorOpenRange < currentYRotation - doorOpenSpeed))
+            {
+                isOpened = true;
+                doorCloseAlarm = doorCloseAlarmDuration;
+            }
         }
+
+        if (doorCloseAlarm <= 0f && isOpened)
+        {
+            if (currentYRotation < 180 - doorCloseSpeed)
+            {
+                currentYRotation += doorCloseSpeed;
+            }
+
+            if (currentYRotation > 180 + doorCloseSpeed)
+            {
+                currentYRotation -= doorCloseSpeed;
+            }
+
+            if (!(currentYRotation < 180 - doorCloseSpeed) && !(currentYRotation > 180 + doorCloseSpeed))
+            {
+                GetComponent<scr_doorGuide>().enabled = false;
+            }
+        }
+
+        doorCloseAlarm--;
 
         transform.rotation = Quaternion.Euler(0f, currentYRotation, 0f);
 	}
---
+++ Unity/Assets/scr_wheelchair.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_wheelchair : MonoBehaviour {
+
+    // Initialize the public variables
+    public GameObject guide;
+    public GameObject guideWheelchairModel;
+    public int waypointTrigger;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (guide.GetComponent<scr_pathFinder>().currentWaypoint > waypointTrigger)
+        {
+            GetComponent<scr_pathFinder>().enabled = true;
+            guideWheelchairModel.SetActive(true);
+            Destroy(guide.gameObject);
+        }
+	}
+}
