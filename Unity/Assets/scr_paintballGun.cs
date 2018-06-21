f45b492ac316e72e0920a19a4a89922d681e6f7d
tobiasBrugel <hedgefoggames@outlook.com>
2018-06-21 12:11:48 +0200

    Update 7

---
+++ Unity/Assets/scr_paintballGun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_paintballGun : MonoBehaviour {
+
+	// Initialize the public variables
+	public GameObject projectile;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch)
+		{
+			GameObject i = Instantiate(projectile, transform) as GameObject;
+			i.GetComponent<RigidBody>().
+		}
+	}
+}
